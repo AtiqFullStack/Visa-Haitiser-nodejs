0cf976a49faa95a08c5921e205e8e0284259add8
atiq.fzeetechz@gmail.com <atiq.fzeetechz@gmail.com>
2026-02-13 16:57:47 +0530

    --

--- src/controllers/admin/QrControllers.js
+++ src/controllers/admin/QrControllers.js
@@ -50,6 +50,7 @@ const createQR = async (req, res) => {
         });
 
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             message: error.message,
         });
