ddbafa80d40e0eb07671e60b28e1765192ef9370
atiq.fzeetechz@gmail.com <atiq.fzeetechz@gmail.com>
2026-02-24 10:02:27 +0530

    -payload size

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const routes = require('./src/routes');
 const errorHandler = require('./src/middlewares/errorHandler');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(process.cwd(), 'src/ejs'));
@@ -22,6 +24,8 @@ app.use('/static', express.static(path.join(process.cwd(), 'src/ejs')));
 
 dbConnect();
 app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/uploads", express.static(path.join(process.cwd(), "public")));
 app.use("/public", express.static(path.join(process.cwd(), "public")));
